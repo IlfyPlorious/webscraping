29c130ce694f85c4d28966c7996d766a3712107a
Dragos <sdragosflorian@gmail.com>
2022-04-11 19:31:44 +0300

    Finished extracting containers and put data in directories

--- eMagScraping/main.py
+++ eMagScraping/main.py
@@ -1,58 +1,105 @@
+import os.path
+
 from bs4 import BeautifulSoup
 from distutils import util
 import requests
 from selenium import webdriver
 from requests_html import HTMLSession
 from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-# session = HTMLSession()
-# r = session.get("https://www.emag.ro/")
-# r.html.render()
-# html = r.text
-#
-# soup = BeautifulSoup(html, 'lxml')
-# html = soup.find_all()
-# containers = soup.find_all('h2')
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+CHROME_PATH = '/usr/bin/google-chrome'
+CHROMEDRIVER_PATH = os.path.join(ROOT_DIR, 'chromedriver')
+WINDOW_SIZE = "1920,1080"
 
-browser = webdriver.Chrome(ChromeDriverManager().install())
-browser.get("https://www.emag.ro/")
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--window-size=%s" % WINDOW_SIZE)
+chrome_options.binary_location = CHROME_PATH
 
-browser.set_window_position(0, 0)
-browser.set_window_size(1920, 1080)
+# /home/dragos/Drivers/chromedriver
 
-item = browser.find_element(by=By.XPATH, value="//*[@data-id='23']")
-# produse = browser.find_element(by=By.XPATH, value="//*[@class='em em-burger']")
-# produse.click()
-hover = ActionChains(browser).move_to_element(item)
-hover.perform()
+def getPages(label):
+    webpages = []
+    classtag = ''
 
-promo = browser.find_element(by=By.XPATH, value="//*[@class='label label-success']")
-promo.click()
+    if label == 'Promo':
+        classtag = 'danger'
+    else:
+        classtag = 'success'
 
-page = browser.page_source
+    # browser = webdriver.Chrome(ChromeDriverManager().install())
+    browser = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,
+                               chrome_options=chrome_options)
+    browser.get("https://www.emag.ro/")
 
-print(page)
+    browser.set_window_position(0, 0)
+    browser.set_window_size(1920, 1080)
 
-browser.quit()
-# print(item)
+    item = browser.find_element(by=By.XPATH, value="//*[@data-id='23']")
 
-# html = browser.page_source
-#
-# soup = BeautifulSoup(html, 'lxml')
-# container = soup.find('div', 'card-v2')
-# topFav = container.find_all("span", string='Top Favorite')
-#
-# print(container)
-# caut spanurile care contin Top Favorite si verific lungimea resultSetului
-# in functie de care stabilesc daca retin informariile sau nu
+    hover = ActionChains(browser).move_to_element(item)
+    hover.perform()
 
-# for span in topFav:
-#     print(span)
-#     print("\n\n")
+    nou = browser.find_elements(by=By.XPATH, value=f"//*[@class='label label-{classtag}']")
+
+    nou[0].click()
+    webpages.append(browser.page_source)
+    secondpage = browser.find_element(by=By.XPATH, value="//*[@data-page='2']")
+    secondpage.click()
+    webpages.append(browser.page_source)
+
+    for i in range(1, len(nou)):
+        browser.back()
+        item = browser.find_element(by=By.XPATH, value="//*[@data-id='23']")
+        hover = ActionChains(browser).move_to_element(item)
+        hover.perform()
+        nou = browser.find_elements(by=By.XPATH, value=f"//*[@class='label label-{classtag}']")
+        nou[i].click()
+        webpages.append(browser.page_source)
+        secondpage = browser.find_element(by=By.XPATH, value="//*[@data-page='2']")
+        secondpage.click()
+        webpages.append(browser.page_source)
+
+    browser.quit()
+    return webpages
+
+
+def getProduseFromPage(page):
+    soup = BeautifulSoup(page, 'lxml')
+    containers = soup.find_all('div', 'card-item card-standard js-product-data')
+
+    produse = []
+
+    for container in containers:
+        if container.find('span', 'card-v2-badge-cmp badge commercial-badge') is not None:
+            badge = container.find('span', 'card-v2-badge-cmp badge commercial-badge').text
+        else:
+            badge = 'no'
+
+        if 'Top Favorite' in badge.strip() or 'Super Pret' in badge.strip():
+            categorie = badge.strip()
+            produs_a = container.find('a', 'card-v2-title semibold mrg-btm-xxs js-product-url')
+            produs = produs_a.text
+            link = produs_a.get('href')
+
+            produs = {
+                'categorie': badge.strip(),
+                'produs': produs.strip(),
+                'link': link
+            }
+
+            produse.append(produs)
+
+    return produse
 
 
 
+#??? why it's not working
 
 
+f = open("page.txt", "r")
+print(getProduseFromPage(f))
